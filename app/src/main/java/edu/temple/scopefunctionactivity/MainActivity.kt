fd64fb3819f90c141a2a0185e4921d27a88b1764
jslootsky <joshuaslootsky@gmail.com>
2025-02-12 16:09:42 -0500

    Created alternate versions of the first two functions by converting them to SEFs using syntax functions

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -13,10 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        val array1: List<Double> = List(5) { it * 1.1 }
+        val array2: List<Double> = List(5) { it * 3.0 }
         // You can test your helper functions by  calling them from onCreate() and
         // printing their output to the Log, which is visible in the LogCat:
         // eg. Log.d("function output", getTestDataArray().toString())
+        Log.d("function 1 output original", getTestDataArray().toString())
+        Log.d("function 1 output modified", getTestDataArray2().toString())
 
+        Log.d("function 2 output original", averageLessThanMedian(array1).toString())
+        Log.d("function 2 output original", averageLessThanMedian2(array1).toString())
     }
 
 
@@ -33,6 +40,9 @@ class MainActivity : AppCompatActivity() {
         return testArray
     }
 
+    private fun getTestDataArray2() : List<Int> =
+        MutableList(10){ Random.nextInt() }.apply{this.sort()}
+
     // Return true if average value in list is greater than median value, false otherwise
     private fun averageLessThanMedian(listOfNumbers: List<Double>): Boolean {
         val avg = listOfNumbers.average()
@@ -44,6 +54,15 @@ class MainActivity : AppCompatActivity() {
 
         return avg < median
     }
+
+    private fun averageLessThanMedian2(listOfNumbers: List<Double>) : Boolean =
+        listOfNumbers.average() < listOfNumbers.sorted().let{ sortedList ->
+                if(sortedList.size % 2 == 0){
+                    (sortedList[sortedList.size / 2] + sortedList[(sortedList.size - 1) / 2]) / 2
+                }else{
+                    sortedList[sortedList.size / 2]
+                }
+        }
 
     // Create a view from an item in a collection, but recycle if possible (similar to an AdapterView's adapter)
     private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
